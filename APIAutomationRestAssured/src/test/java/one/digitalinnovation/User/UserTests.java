976295c1ee7a24db5c8e1181c0d82d55ed2407a3
danielle-victoria <danielle.victoria.eustaquio@gmail.com>
2023-01-22 19:24:24 -0300

    Aula 1 sobre Testes Automatizados com Rest Assured

---
+++ APIAutomationRestAssured/src/test/java/one/digitalinnovation/User/UserTests.java
@@ -0,0 +1,54 @@
+package one.digitalinnovation.User;
+
+import io.restassured.RestAssured;
+import io.restassured.filter.log.ErrorLoggingFilter;
+import io.restassured.filter.log.RequestLoggingFilter;
+import io.restassured.filter.log.ResponseLoggingFilter;
+import io.restassured.http.ContentType;
+import io.restassured.response.Response;
+import io.restassured.specification.RequestSpecification;
+import org.junit.jupiter.api.*;
+
+import com.github.javafaker.Faker;
+import one.digitalinnovation.Entities.User;
+
+import static io.restassured.RestAssured.given;
+import static io.restassured.config.LogConfig.logConfig;
+import static io.restassured.module.jsv.JsonSchemaValidator.*;
+import static org.hamcrest.Matchers.*;
+
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+public class UserTests {
+
+    private static User user;
+    public Faker faker;
+    public static RequestSpecification request;
+
+    @BeforeAll
+    public void setup(){
+        RestAssured.baseURI = "https://petstore.swagger.io/v2";
+
+        faker = new Faker();
+        //Setando os dados
+        user = new User(faker.name().username(),
+                faker.name().firstName(),
+                faker.name().lastName(),
+                faker.internet().safeEmailAddress(),
+                faker.internet().password(8,10),
+                faker.phoneNumber().toString());
+        
+    }
+
+    @BeforeEach
+    void setRequest(){
+        request = given()
+                 .header("api-key", "special-key")
+                 .contentType(ContentType.JSON);
+    }
+
+    @Test
+    public void CreateNewUser_WithValidData_ReturnOk(){
+        
+    }
+}
+    
