d57bf9fa3ee34e8dc3efff37cdd1900a8e0564da
danielle-victoria <danielle.victoria.eustaquio@gmail.com>
2023-01-25 00:16:06 -0300

    Testes com Selenium WebDriver - Actions - pt2

--- automation-pratice-selenium/src/test/java/one/digitalinnovation/pages/DressesPage.java
+++ automation-pratice-selenium/src/test/java/one/digitalinnovation/pages/DressesPage.java
@@ -5,15 +5,22 @@ import org.openqa.selenium.By;
 public class DressesPage extends BasePage{
 
     //Locators
-    private By menuDressesLocator = By.cssSelector("#cm-header-navigation > div.cm-layout-max.mx-auto.flex.justify-center.items-center.gap-2 > div.overflow-hidden.-my-4 > div > div:nth-child(4) > a > span.whitespace-nowrap.group-1-hover/:text-/[color/:var/(--cm-color-hover-link/)/]");
+    private By menuDressesLocator = By.xpath("/html/body/div[1]/div/div[2]/div/div[2]/div[1]/div[1]/div/div[4]/a/span[1]");
+    private By submenuCasualDressesLocator = By.xpath("/html/body/div[1]/div/div[2]/div/div[2]/div[5]/div/div/div[1]/a[2]/span");
+    private By titlePageCasualDressesLocator = By.tagName("h1");
 
     public void viewCasualDressesPage(){
         if(super.isDisplayed(menuDressesLocator)){
             super.actionMoveToElementPerform(menuDressesLocator);
+            super.actionMoveToElementClickPerform(submenuCasualDressesLocator);
         }else{
             System.out.println("menu dresses was not found");
         }
 
+    }
+
+    public String getTitlePage(){
+        return super.getText(titlePageCasualDressesLocator);
     }
     
 }
--- automation-pratice-selenium/src/test/java/one/digitalinnovation/tests/DressesPageTest.java
+++ automation-pratice-selenium/src/test/java/one/digitalinnovation/tests/DressesPageTest.java
@@ -1,26 +1,40 @@
 package one.digitalinnovation.tests;
 
+import one.digitalinnovation.pages.*;
 import org.junit.jupiter.api.Test;
+
+import one.digitalinnovation.pages.DressesPage;
+
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 
 public class DressesPageTest {
 
+    private DressesPage dressesPage;
+    private final String URL = "https://noracora.com";
+
     @BeforeEach
     public void setUp() throws Exception{
-
+        this.dressesPage = new DressesPage();
+        this.dressesPage.visit(this.URL);
         
     }
 
     @AfterEach
     public void tearDown() throws Exception{
+        this.dressesPage.quitWebDriver();
         
     }
 
     @Test
     void test(){
-        
+        //when
+        this.dressesPage.viewCasualDressesPage();
+
+        //then
+        //Assertions.assertEquals("Casual Dresses", this.dressesPage.getTitlePage());
+        Assertions.assertFalse(this.URL.equals(dressesPage.getCurrentUrl()));
 
     }
     
